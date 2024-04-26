0994344a7d17d656cac467ece7655d3674097eaa
Mario Calín <mario.calin@mindcurv.com>
2024-04-26 15:32:23 +0200

    Improving test with handler

--- internal/book/handler_test.go
+++ internal/book/handler_test.go
@@ -12,30 +12,6 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-type MockBookService struct {
-	mock.Mock
-}
-
-func (m *MockBookService) GetBooks() ([]Book, error) {
-	args := m.Called()
-	return args.Get(0).([]Book), args.Error(1)
-}
-
-func (m *MockBookService) GetBookByID(id BookId) (*Book, error) {
-	args := m.Called(id)
-	return args.Get(0).(*Book), args.Error(1)
-}
-
-func (m *MockBookService) CreateBook(cmd *CreateBookCommand) (*Book, error) {
-	args := m.Called(cmd)
-	return args.Get(0).(*Book), args.Error(1)
-}
-
-func (m *MockBookService) UpdateBook(id BookId, cmd *UpdateBookCommand) error {
-	args := m.Called(cmd)
-	return args.Error(0)
-}
-
 func setupRouter() *gin.Engine {
 	gin.SetMode(gin.TestMode)
 	router := gin.New()
@@ -43,13 +19,13 @@ func setupRouter() *gin.Engine {
 }
 
 func TestCreateBookHandler(t *testing.T) {
-	mockSvc := new(MockBookService)
+	mockSvc := NewMockBookService(t)
 	router := setupRouter()
 	router.POST("/book", createBookHandler(mockSvc))
 
 	t.Run("Success", func(t *testing.T) {
 
-		bookDate, _ := time.Parse(time.RFC3339, "2022-01-01T15:04:05Z")
+		bookDate, _ := time.Parse(time.DateOnly, "2022-01-01")
 
 		book := Book{
 			ID:          uuid.New(),
@@ -61,7 +37,7 @@ func TestCreateBookHandler(t *testing.T) {
 
 		mockSvc.On("CreateBook", mock.Anything).Return(&book, nil)
 
-		body := `{"name":"Example","publish_date":"2022-01-01T15:04:05Z","categories":["fiction"],"description":"A book description"}`
+		body := `{"name":"Example","publish_date":"2022-01-01","categories":["fiction"],"description":"A book description"}`
 		req := httptest.NewRequest("POST", "/book", bytes.NewBufferString(body))
 		req.Header.Set("Content-Type", "application/json")
 		w := httptest.NewRecorder()
